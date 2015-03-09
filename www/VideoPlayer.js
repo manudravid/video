42fd92c668e23940999c36c40d1bf3f52179e10a
Manu Palassery <manudravid@gmail.com>
2015-03-09 13:03:14 +0530

    Update VideoPlayer.js

--- www/VideoPlayer.js
+++ www/VideoPlayer.js
@@ -1,10 +1,30 @@
-
 var exec = require("cordova/exec");
 
-var NewActivty = {
-    play: function(url,width,height) {
-        exec(null, null, "VideoPlayer", "play", [url,width,height]);
-    }
-};
+module.exports = {
 
-module.exports = VideoPlayer;
+    DEFAULT_OPTIONS: {
+        volume: 1.0,
+        scalingMode: 1
+    },
+
+    SCALING_MODE: {
+        SCALE_TO_FIT: 1,
+        SCALE_TO_FIT_WITH_CROPPING: 2
+    },
+
+    play: function (path, options, successCallback, errorCallback) {
+        options = this.merge(this.DEFAULT_OPTIONS, options);
+        exec(successCallback, errorCallback, "VideoPlayer", "play", [path, options]);
+    },
+
+    merge: function () {
+        var obj = {};
+        Array.prototype.slice.call(arguments).forEach(function(source) {
+            for (var prop in source) {
+                obj[prop] = source[prop];
+            }
+        });
+        return obj;
+    }
+
+};
